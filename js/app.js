939a37eda5a59d6cc827279712b8b6537adc09b7
yuna3741 <yuna3741@naver.com>
2024-07-03 14:47:27 +0900

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,70 @@
 $(function(){
+  $(".ham").click(function(){
+    $(".menu").css({transform:'translateY(0px)'}),
+    $(".ham").css({display:'none'})
+  }),
+
+  $(".close").click(function(){
+    $(".menu").css({transform:'translateY(-1000px)'}),
+    $(".ham").css({display:'block'})
+  }),
+
+  $("header .h_inner nav ul li").mouseover(function(){
+   $(".dot",this).css({transform:'translateY(0px)'})
+  }),
+  $("header .h_inner nav ul li").mouseout(function(){
+    $(".dot",this).css({transform:'translateY(-1000px)'})
+   }),
+
+   $(".menu .m_inner ul li").mouseover(function(){
+    $(".spot",this).css({transform:'translateY(0px)'})
+   }),
+   $(".menu .m_inner ul li").mouseout(function(){
+     $(".spot",this).css({transform:'translateY(-1000px)'})
+    }),
+
+  $(".t1_slick").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  }),
+
+
+
+  $(".t2_slick").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  }),
+
+  $(window).resize(function(){
+    let w = innerWidth
+
+    if(w > 1240){
+      $(".ham").css({display:'none'})
+    }else{
+      $(".ham").css({display:'block'})
+    }
+  })
+
+
+
+  
+
+
+
+  /////////////
+});
+
+
+
+
+
+/*$(function(){
     $(".i_slide").slick({
         responsive: [
             {
@@ -11,4 +77,6 @@ $(function(){
               }}]
     });
    
-})
+})*/
+
+
